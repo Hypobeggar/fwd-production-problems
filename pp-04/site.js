e2cb6801059d08cbf506cef785663e4540db0480
Hypobeggar <89600337+Hypobeggar@users.noreply.github.com>
2021-11-15 22:57:29 -0600

    Step 3, handle non-number values

--- pp-04/site.js
+++ pp-04/site.js
@@ -6,7 +6,8 @@
 
 
 
-/*function double(num) {
+/* STEP 1
+function double(num) {
   x = 5;
   return x;
 }
@@ -14,16 +15,35 @@
 double(6);
 console.log('The value of x is', x, '-- it should be 5.');
 */
-function myfunction() {
-  function double() {
+
+/* STEP 2
+function myfunction () {
+  function double(num) {
     var x = 5;
     return x;
   }
 }
 
+double(6);
+console.log('The value of x is', x, '-- it should be 5.');
+*/
+
+// STEP 3
+function myfunction(data) {
+  return double(data);
+  function double(num) {
+    if (typeof(num) !== 'number') {
+      console.log("num must be a number")
+      return;
+    }
+    var x = 5;
+    x = num * 2;
+    return x;
+  }
+}
 
 
-console.log('The value of x is', myfunction.double, '-- it should be 5.');
+console.log('The value of x is', myfunction(5), '-- it should be 5.');
 /*
   Step 2. Rewrite the JavaScript `double()` function above so that
   it does not pollute the global namespace: that is, neither its
